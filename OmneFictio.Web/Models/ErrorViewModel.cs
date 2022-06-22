40928e487f6102db3837afe673fb2d802ea89cf0
İrşat Akdeniz <irsat000@gmail.com>
2022-06-22 16:46:49 +0300

    first commit

---
+++ OmneFictio.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OmneFictio.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ OmneFictio.Web/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$('.ppic-wrap, .dropdown_icon-wrap').click(function(){
+    if($('.account-dropdown').hasClass('d-block')){
+        $('.account-dropdown').removeClass('opacity-100');
+        setTimeout(function(){
+            $('.account-dropdown').removeClass('d-block');
+        }, 100);
+    }
+    else{
+        $('.account-dropdown').addClass('d-block');
+        setTimeout(function(){
+            $('.account-dropdown').addClass('opacity-100');
+        }, 100);
+    }
+});
+
+$('body').click(function(event) {
+    var target = $(event.target);
+    if($('.account-dropdown').hasClass('d-block') && !target.parents('.account-cont').length){
+        $('.account-dropdown').removeClass('opacity-100');
+        setTimeout(function(){
+            $('.account-dropdown').removeClass('d-block');
+        }, 100);
+    }
+});
