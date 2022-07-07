467dd971b9e4003f63a863e1a540c1feb189dc49
İrşat Akdeniz <irsat000@gmail.com>
2022-07-07 19:22:16 +0300

    splitting pages and new homepage 1

---
+++ OmneFictio.Web/wwwroot/js/read.js
@@ -0,0 +1,168 @@
+function capitalizeFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+$('#orderby-btn, #sb-close').click(function(){
+    if($('#orderby-modal').hasClass('d-flex')){
+        $('#orderby-modal').removeClass('d-flex');
+        $('#orderby-modal').removeClass('opacity-100');
+        $('.body-outside').removeClass('d-block');
+    }
+    else{
+        $('#orderby-modal').addClass('d-flex');
+        $('.body-outside').addClass('d-block');
+        setTimeout(function(){
+            $('#orderby-modal').addClass('opacity-100');
+        }, 100);
+    }
+});
+$('#po-type, #t-close').click(function(){
+    if($('#type-modal').hasClass('d-flex')){
+        $('#type-modal').removeClass('d-flex');
+        $('#type-modal').removeClass('opacity-100');
+        $('.body-outside').removeClass('d-block');
+    }
+    else{
+        $('#type-modal').addClass('d-flex');
+        $('.body-outside').addClass('d-block');
+        setTimeout(function(){
+            $('#type-modal').addClass('opacity-100');
+        }, 100);
+    }
+});
+
+$('#po-filter, #f-close').click(function(){
+    if($('#filter-modal').hasClass('d-flex')){
+        $('#filter-modal').removeClass('d-flex');
+        $('#filter-modal').removeClass('opacity-100');
+        $('.body-outside').removeClass('d-block');
+    }
+    else{
+        $('#filter-modal').addClass('d-flex');
+        $('.body-outside').addClass('d-block');
+        setTimeout(function(){
+            $('#filter-modal').addClass('opacity-100');
+        }, 100);
+    }
+});
+
+
+$('#f-includetagbtn').click(function(){
+    openFilterTagDD("include");
+});
+$('#f-excludetagbtn').click(function(){
+    openFilterTagDD("exclude");
+});
+$('.f-modaldarkness, #f-taggdd-close').click(function(){
+    openFilterTagDD("close");
+});
+function openFilterTagDD(action){
+    $('#taggdd-searchbar').val("");
+    $("#filter-taggddmodal > ul > li").each(function(){
+        $(this).show()
+    });
+    if($('#filter-taggddmodal').hasClass('d-flex')){
+        $('#filter-taggddmodal').removeClass('d-flex');
+        $('.f-modaldarkness').removeClass('d-block');
+    }
+    else{
+        $('#filter-taggddmodal').addClass('d-flex');
+        $('.f-modaldarkness').addClass('d-block');
+    }
+    if(action == "include") {
+        $('#f-tagdd-include').addClass('d-block');
+        $('#f-tagdd-exclude').removeClass('d-block');
+    }
+    else if(action == "exclude"){
+        $('#f-tagdd-include').removeClass('d-block');
+        $('#f-tagdd-exclude').addClass('d-block');
+    }
+}
+
+$('#f-tagdd-include > li, #f-tagdd-exclude > li').click(function(){
+    var tagname = $(this).attr('data-tagddvalue');
+    var tagnamedisplay = capitalizeFirstLetter(tagname.replace('_', ' '));
+    if($(this).parents('#f-tagdd-include').length){
+        $('.f-tagsincexc span[data-ftag="'+tagname+'"]').remove();
+        $('.f-tagsincexc input[data-ftagref="'+tagname+'"]').remove();
+        $(".f-tags_include").append(`<span data-ftag="`+tagname+`">`+tagnamedisplay+`<i class="bi bi-x-circle f-removetagbtn"></i></span>
+                                        <input type="hidden" name="taginclude" value="`+tagname+`" data-ftagref="`+tagname+`"/>`);
+    }
+    else{
+        $('.f-tagsincexc span[data-ftag="'+tagname+'"]').remove();
+        $('.f-tagsincexc input[data-ftagref="'+tagname+'"]').remove();
+        $(".f-tags_exclude").append(`<span data-ftag="`+tagname+`">`+tagnamedisplay+`<i class="bi bi-x-circle f-removetagbtn"></i></span>
+                                        <input type="hidden" name="tagexclude" value="`+tagname+`" data-ftagref="`+tagname+`"/>`);
+    }
+    openFilterTagDD("close");
+});
+
+$("#taggdd-searchbar").keyup(function(){
+    var filter = $(this).val();
+    $("#filter-taggddmodal > ul > li").each(function () {
+        if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+            $(this).hide();
+        } else {
+            $(this).show()
+        }
+    });
+});
+
+//appended elements require this method
+$(document).on('click', '.f-tagsincexc span', function(event){
+    var target = $(event.target);
+    if(target.hasClass('f-removetagbtn')){
+        var tagname = $(this).attr('data-ftag');
+        $(this).remove();
+        $('.f-tagsincexc > input[data-ftagref="'+tagname+'"]').remove();
+    }
+    else{
+        var removetagbtn = $(this).children('.f-removetagbtn');
+        if(!removetagbtn.hasClass('d-flex')){
+            $('.f-removetagbtn').removeClass('d-flex');
+            removetagbtn.addClass('d-flex');
+        }
+        else{
+            removetagbtn.removeClass('d-flex');
+        }
+    }
+});
+$(document).on('click', '.filter-cont *', function(event){
+    var target = $(event.target);
+    if($('#filter-modal').hasClass('d-flex') && !target.parents('.f-tagsincexc').length && $('.f-removetagbtn').hasClass('d-flex')){
+        $('.f-removetagbtn').removeClass('d-flex');
+    }
+});
+
+//Changes the condition of types in filter modal. Also changes input that will be sent to back end.
+$('.f-typebtns').click(function(){
+    var buttontext = $(this).val();
+    var type = $(this).attr('data-type');
+    if($(this).hasClass('f-type_active')){
+        $(this).removeClass('f-type_active');
+        $(this).val(buttontext.replace('-', '+'));
+        $('.f-type_cont > input[data-typeref="'+type+'"]').val(type + '-0');
+    }
+    else{
+        $(this).addClass('f-type_active');
+        $(this).val(buttontext.replace('+', '-'));
+        $('.f-type_cont > input[data-typeref="'+type+'"]').val(type + '-1');
+    }
+});
+
+//Resets the filters
+$('#f-resetbtn').click(function(){
+    //type
+    $('.f-typebtns').removeClass('f-type_active');
+    $('.f-typebtns').each(function(i, x){
+        var buttontext = $(x).val();
+        $(x).val(buttontext.replace('-', '+'));
+        var type = $(x).attr('data-type');
+        $('.f-type_cont > input[data-typeref="'+type+'"]').val(type + '-0');
+    });
+    //tags
+    $('.f-tagsincexc').empty();
+    //options
+    $(".f-options > select").val('0');
+    $("#checkboxInputOverride").prop("checked", false);
+});
