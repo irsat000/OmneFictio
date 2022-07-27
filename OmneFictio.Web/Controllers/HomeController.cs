81884e35a81727a0c2f502e0bd62ecf00d81541a
İrşat Akdeniz <irsat000@gmail.com>
2022-07-27 13:24:57 +0300

    a couple fixes

--- OmneFictio.Web/Controllers/HomeController.cs
+++ OmneFictio.Web/Controllers/HomeController.cs
@@ -24,22 +24,22 @@ public class HomeController : Controller
 
     public async Task<IActionResult> Read(string? type)
     {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
         List<PostRead1>? posts = new List<PostRead1>();
         string postsUrl = "https://localhost:7022/posts";
         using (HttpClient httpClient = new HttpClient())
         {
-            using (HttpResponseMessage response = await httpClient.GetAsync(postsUrl))
-            {
-                using (HttpContent content = response.Content)
-                {
-                    var raw = await content.ReadAsStringAsync();
-                    posts = JsonConvert.DeserializeObject<List<PostRead1>>(raw);
-                }
-            }
+            string raw = httpClient.GetStringAsync(postsUrl).Result;
+            posts = JsonConvert.DeserializeObject<List<PostRead1>>(raw);
         }
         ReadViewmodel viewModel = new ReadViewmodel{
             posts = posts
         };
+
+        stopwatch.Stop(); 
+        Console.WriteLine($"Controllerda geçen zaman: {stopwatch.Elapsed}");
         return View(viewModel);
     }
 
