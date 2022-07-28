78be9d27ab68c572f779d4942be5c4b12cbc44f3
İrşat Akdeniz <irsat000@gmail.com>
2022-07-28 20:13:21 +0300

    design fixes

--- OmneFictio.Web/Controllers/HomeController.cs
+++ OmneFictio.Web/Controllers/HomeController.cs
@@ -24,22 +24,28 @@ public class HomeController : Controller
 
     public async Task<IActionResult> Read(string? type)
     {
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
+        Stopwatch stopwatch1 = new Stopwatch();
+        Stopwatch stopwatch2 = new Stopwatch();
+        Stopwatch stopwatch3 = new Stopwatch();
 
         List<PostRead1>? posts = new List<PostRead1>();
         string postsUrl = "https://localhost:7022/posts";
         using (HttpClient httpClient = new HttpClient())
         {
+            stopwatch2.Start();
             string raw = httpClient.GetStringAsync(postsUrl).Result;
+            stopwatch2.Stop();
+
+            stopwatch3.Start();
             posts = JsonConvert.DeserializeObject<List<PostRead1>>(raw);
+            stopwatch3.Stop();
         }
         ReadViewmodel viewModel = new ReadViewmodel{
             posts = posts
         };
 
-        stopwatch.Stop(); 
-        Console.WriteLine($"Controllerda geçen zaman: {stopwatch.Elapsed}");
+        Console.WriteLine($"getting raw data: {stopwatch2.Elapsed}");
+        Console.WriteLine($"deserializing: {stopwatch3.Elapsed}");
         return View(viewModel);
     }
 
