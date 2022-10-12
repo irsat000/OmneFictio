b04fcc7089c908ef9365a3291ee6aa1defcf121f
İrşat Akdeniz <irsat000@gmail.com>
2022-10-12 17:55:16 +0300

    webapi auth controller

--- OmneFictio.WebApi/Controllers/AuthController.cs
+++ OmneFictio.WebApi/Controllers/AuthController.cs
@@ -26,8 +26,10 @@ public class AuthController : ControllerBase
     private readonly OmneFictioContext _db;
     private readonly IMapper _mapper;
     private readonly ILogger<ReadingController> _logger;
+    private readonly IConfiguration configuration;
+    JwtSecurityTokenHandler _jwtHandler = new JwtSecurityTokenHandler();
 
-    public AuthController(ILogger<ReadingController> logger, IMapper mapper, OmneFictioContext db)
+    public AuthController(ILogger<ReadingController> logger, IMapper mapper, OmneFictioContext db, IConfiguration iConfig)
     {
         _logger = logger;
         _mapper = mapper;
@@ -36,6 +38,151 @@ public class AuthController : ControllerBase
             throw new InvalidOperationException("Mapper not found");
         }
         _db = db;
+        configuration = iConfig;
     }
-    
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] AccountDtoRead_2 request)
+    {
+        var securityToken = Encoding.ASCII.GetBytes(configuration.GetSection("Token").Value);
+        //Authentication
+        var checkUser = await _db.Accounts.SingleOrDefaultAsync(x => x.username == request.Username);
+        if (checkUser == null || !BC.Verify(request.Pw, checkUser.pw))
+        {
+            return NotFound();
+        }
+        //Login
+        var createToken = UserController.CreateUserToken(checkUser, securityToken);
+        if (createToken == null)
+        {
+            return NotFound();
+        }
+        return Ok(new { jwt = createToken });
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register([FromBody] AccountDtoWrite_1 request)
+    {
+        var securityToken = Encoding.ASCII.GetBytes(configuration.GetSection("Token").Value);
+        //Input validation
+        Regex usernameRegex = new Regex(@"[A-Za-z0-9_]{3,30}");
+        if (!usernameRegex.IsMatch(request.Username) ||
+            await _db.Accounts.AnyAsync(a => a.username == request.Username) ||
+            request.Pw!.Contains(" ") ||
+            request.Pw!.Length < 6)
+        {
+            return BadRequest();
+        }
+        //Fill an account object
+        string passwordHash = BC.HashPassword(request.Pw);
+        Account newAccount = new Account();
+        newAccount.username = request.Username;
+        newAccount.pw = passwordHash;
+        newAccount.email = request.Email;
+        newAccount.externalType = "native";
+        newAccount.emailValid = false;
+        if (request.AllowAdultContent != null)
+        {
+            newAccount.allowAdultContent = request.AllowAdultContent;
+        }
+        else
+        {
+            newAccount.allowAdultContent = false;
+        }
+        if (await _db.Languages.AnyAsync(l => l.id == request.PrefLanguageId))
+        {
+            newAccount.prefLanguageId = request.PrefLanguageId;
+        }
+        //Save user in the database
+        try
+        {
+            await _db.Accounts.AddAsync(newAccount);
+            await _db.SaveChangesAsync();
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+        //Login
+        var user = await _db.Accounts.SingleOrDefaultAsync(x => x.username == request.Username);
+        var createToken = UserController.CreateUserToken(user!, securityToken);
+        if (createToken == null)
+        {
+            return Ok();
+        }
+        return Ok(new { jwt = createToken });
+    }
+
+    [HttpPost("Signin-External")]
+    public async Task<IActionResult> Signin_External([FromBody] string token)
+    {
+        var securityToken = Encoding.ASCII.GetBytes(configuration.GetSection("Token").Value);
+        var validationSettings = new ValidationSettings
+        {
+            Audience = new string[]
+                {"589655350334-bdk3rgelbo3i4otkj08f2hvc69g1lql4.apps.googleusercontent.com"},
+            IssuedAtClockTolerance = TimeSpan.FromSeconds(100),
+            ExpirationTimeClockTolerance = TimeSpan.FromSeconds(100)
+        };
+        try
+        {
+            JsonWebSignature.Payload payload = await GoogleJsonWebSignature.ValidateAsync(token, validationSettings);
+        }
+        catch (InvalidJwtException) { return BadRequest(); }
+
+        var jwt = _jwtHandler.ReadJwtToken(token);
+
+        //Gets the Id given by google
+        var extId = jwt.Claims.First(claim => claim.Type == "sub").Value;
+
+        //Creates an account if there is no one with this ID from google
+        //If there is one already, it skips to login
+        if (await _db.Accounts.SingleOrDefaultAsync(a => a.externalId == extId && a.externalType == "google") == null)
+        {
+            Random random = new Random();
+            var email = jwt.Claims.First(claim => claim.Type == "email").Value;
+            var profilePic = jwt.Claims.First(claim => claim.Type == "picture").Value;
+            var name = Regex.Replace(jwt.Claims.First(claim => claim.Type == "name").Value, @"\s+", "");
+            var username = name;
+            while (await _db.Accounts.AnyAsync(a => a.username == username))
+            {
+                username = name + random.Next(100, 1000).ToString();
+            }
+            string passwordHash = BC.HashPassword(UserController.GeneratePassword(16, 8));
+
+            Account newAccount = new Account();
+            newAccount.username = username;
+            newAccount.email = email;
+            newAccount.externalId = extId;
+            newAccount.profilePic = profilePic;
+            newAccount.displayName = name;
+            newAccount.pw = passwordHash;
+            newAccount.externalType = "google";
+            newAccount.allowAdultContent = false;
+            newAccount.prefLanguageId = null;
+            newAccount.emailValid = true;
+
+            try
+            {
+                await _db.Accounts.AddAsync(newAccount);
+                await _db.SaveChangesAsync();
+            }
+            catch (DbEntityValidationException)
+            {
+                return BadRequest();
+            }
+        }
+
+        //Gets the user for authorization
+        var user = await _db.Accounts.SingleOrDefaultAsync(a => a.externalId == extId && a.externalType == "google");
+        if (user == null)
+            return BadRequest();
+
+        var createToken = UserController.CreateUserToken(user, securityToken);
+        if (createToken == null)
+        {
+            return BadRequest();
+        }
+        return Ok(new { jwt = createToken });
+    }
 }
---
+++ OmneFictio.WebApi/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+using OmneFictio.WebApi.Entities;
+
+namespace OmneFictio.WebApi.Controllers;
+
+public class UserController : ControllerBase
+{
+    public UserController()
+    {
+        
+    }
+    public static string? CreateUserToken(Account user, byte[] securityToken){
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor {
+            Subject = new ClaimsIdentity(new List<Claim>(){
+                new Claim(ClaimTypes.NameIdentifier, user.id.ToString()),
+                new Claim(ClaimTypes.Name, user.username),
+                new Claim(ClaimTypes.Email, user.email),
+                new Claim(ClaimTypes.Actor, user.profilePic ?? "noimage")
+            }),
+            Issuer = "OmneFictio.com",
+            Expires = DateTime.UtcNow.AddDays(30),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(securityToken), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+        return token;
+    }
+
+    public static string GeneratePassword(int length, int numberOfNonAlphanumericCharacters)
+    {
+        char[] Punctuations = "!@#$%^&*()_-+=[{]};:>|./?".ToCharArray();
+        if (length < 1 || length > 128)
+        {
+            throw new ArgumentException(nameof(length));
+        }
+
+        if (numberOfNonAlphanumericCharacters > length || numberOfNonAlphanumericCharacters < 0)
+        {
+            throw new ArgumentException(nameof(numberOfNonAlphanumericCharacters));
+        }
+
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            var byteBuffer = new byte[length];
+
+            rng.GetBytes(byteBuffer);
+
+            var count = 0;
+            var characterBuffer = new char[length];
+
+            for (var iter = 0; iter < length; iter++)
+            {
+                var i = byteBuffer[iter] % 87;
+
+                if (i < 10)
+                {
+                    characterBuffer[iter] = (char)('0' + i);
+                }
+                else if (i < 36)
+                {
+                    characterBuffer[iter] = (char)('A' + i - 10);
+                }
+                else if (i < 62)
+                {
+                    characterBuffer[iter] = (char)('a' + i - 36);
+                }
+                else
+                {
+                    characterBuffer[iter] = Punctuations[i - 62];
+                    count++;
+                }
+            }
+
+            if (count >= numberOfNonAlphanumericCharacters)
+            {
+                return new string(characterBuffer);
+            }
+
+            int j;
+            var rand = new Random();
+
+            for (j = 0; j < numberOfNonAlphanumericCharacters - count; j++)
+            {
+                int k;
+                do
+                {
+                    k = rand.Next(0, length);
+                }
+                while (!char.IsLetterOrDigit(characterBuffer[k]));
+
+                characterBuffer[k] = Punctuations[rand.Next(0, Punctuations.Length)];
+            }
+
+            return new string(characterBuffer);
+        }
+    }
+}
