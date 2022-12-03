82161782d9a18455d97e29540a1daa0ec988b305
İrşat Akdeniz <irsat000@gmail.com>
2022-12-03 15:57:01 +0300

    profile stats and nav

--- OmneFictio.Web/wwwroot/js/profile.js
+++ OmneFictio.Web/wwwroot/js/profile.js
@@ -1,14 +1,15 @@
 
 $(document).ready(function () {
-    let profileTabLinks = document.querySelectorAll('.profile-tabs a');
+    let profileTabLinks = document.querySelectorAll('.profile-tabs-cont a');
 
     const pPath = "/u/" + getPathPart(2) + "/";
-    let pTab = "stats";
+    const getTab = getPathPart(3);
+    let pTab = getTab === "" ? "posts" : getTab;
 
     profileTabLinks.forEach(function (a) {
         a.addEventListener("click", function () {
             pTab = a.getAttribute('data-tab');
-            window.history.pushState(null, null, pPath+pTab);
+            window.history.pushState(null, null, pPath + pTab);
             changeProfileTab(pTab);
         });
     });
@@ -22,8 +23,8 @@ $(document).ready(function () {
             }
         });
         //Activating new tab (Button only)
-        document.querySelector('.profile-tabs a[data-tab="' + tab + '"]').classList.add('active');
-
+        document.querySelector('.profile-tabs-mobile a[data-tab="' + tab + '"]').classList.add('active');
+        document.querySelector('.profile-tabs-desktop a[data-tab="' + tab + '"]').classList.add('active');
         //Filling the tabs
         switch (tab) {
             case "posts":
@@ -32,13 +33,30 @@ $(document).ready(function () {
                 break;
             case "saved":
                 break;
-            case "liked":
+            case "followed":
                 break;
             case "friends":
                 break;
             default:
-                //stats
                 break;
         }
     }
+
+    
+    $(window).resize(function () {
+        /*if ($(window).width() > 991) {
+            //check if user clicked stats in mobile design and got back to desktop
+            //(desktop doesn't have stats button, stats are on the left)
+            const a = document.querySelector('.profile-tabs-mobile a[data-tab="stats"]');
+            if (a.classList.contains("active")) {
+                a.classList.remove("active");
+                document.querySelector('.profile-tabs-mobile a[data-tab="posts"]').classList.add('active');
+                document.querySelector('.profile-tabs-desktop a[data-tab="posts"]').classList.add('active');
+                changeProfileTab("posts");
+            }
+        }
+        else {
+
+        }*/
+    });
 });
