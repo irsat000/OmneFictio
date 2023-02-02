d0090d3ab1f952bee17495a15b7e7aa76e494759
irsat000 <irsat000@gmail.com>
2023-02-02 06:30:22 +0300

    index swiper - 1

--- OmneFictio.Web/TypeScript/index.ts
+++ OmneFictio.Web/TypeScript/index.ts
@@ -5,6 +5,7 @@ class ofIndex_GetTopPosts {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    const swiper_images = document.querySelector('.swiper-images') as HTMLDivElement;
 
     fetchTopPosts();
     function fetchTopPosts() {
@@ -31,10 +32,10 @@ document.addEventListener("DOMContentLoaded", function () {
                     for (const post of response.monthsTopPosts) {
                         monthtopBody.appendChild(window.fillPostTemplate(post, false));
                     }
-                    if(response.todaysTopPosts.length < 1){
+                    if (response.todaysTopPosts.length < 1) {
                         //Letting the user know there was no posts
                     }
-                    if(response.monthsTopPosts.length < 1){
+                    if (response.monthsTopPosts.length < 1) {
                         //Letting the user know there was no posts
                     }
                 }
@@ -45,4 +46,50 @@ document.addEventListener("DOMContentLoaded", function () {
             })
             .catch(error => { console.log("Fetch failed -> " + error) });
     }
+
+    document.querySelectorAll('.fswipe-prev i, .fswipe-next i').forEach(btn => {
+        btn.addEventListener('click', () => {
+            //Current active
+            const currentActive = swiper_images.querySelector('a.active') as HTMLElement;
+            const currentIndex = parseInt(currentActive.getAttribute('data-featured')!, 10);
+            //Get all
+            const a_indexes = [...swiper_images.children]
+                .map(a => parseInt(a.getAttribute('data-featured')!, 10));
+            //Swipe
+            const nextIndex = btn.closest('.fswipe-next')
+                ? next_swiper_index(a_indexes, currentIndex)
+                : prev_swiper_index(a_indexes, currentIndex);
+
+            currentActive.classList.remove('active');
+            swiper_images.querySelector('[data-featured="' + nextIndex + '"]')!.classList.add('active');
+        });
+    });
 });
+
+function prev_swiper_index(numbers: number[], num: number): number | null {
+    let prev = Number.POSITIVE_INFINITY;
+    let biggest = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < num && (prev === Number.POSITIVE_INFINITY || numbers[i] > prev)) {
+            prev = numbers[i];
+        }
+        if (numbers[i] > num) {
+            biggest = numbers[i];
+        }
+    }
+    return prev === Number.POSITIVE_INFINITY ? biggest : prev;
+}
+
+function next_swiper_index(numbers: number[], num: number): number | null {
+    let next = Number.NEGATIVE_INFINITY;
+    let smallest = Number.POSITIVE_INFINITY;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > num && (next === Number.NEGATIVE_INFINITY || numbers[i] < next)) {
+            next = numbers[i];
+        }
+        if (numbers[i] < smallest) {
+            smallest = numbers[i];
+        }
+    }
+    return next === Number.NEGATIVE_INFINITY ? smallest : next;
+}
--- OmneFictio.Web/wwwroot/js/index.js
+++ OmneFictio.Web/wwwroot/js/index.js
@@ -4,6 +4,7 @@ class ofIndex_GetTopPosts {
     monthsTopPosts;
 }
 document.addEventListener("DOMContentLoaded", function () {
+    const swiper_images = document.querySelector('.swiper-images');
     fetchTopPosts();
     function fetchTopPosts() {
         const todaytopBody = document.querySelector('.todaytop_body');
@@ -40,4 +41,43 @@ document.addEventListener("DOMContentLoaded", function () {
         })
             .catch(error => { console.log("Fetch failed -> " + error); });
     }
+    document.querySelectorAll('.fswipe-prev i, .fswipe-next i').forEach(btn => {
+        btn.addEventListener('click', () => {
+            const currentActive = swiper_images.querySelector('a.active');
+            const currentIndex = parseInt(currentActive.getAttribute('data-featured'), 10);
+            const a_indexes = [...swiper_images.children]
+                .map(a => parseInt(a.getAttribute('data-featured'), 10));
+            const nextIndex = btn.closest('.fswipe-next')
+                ? next_swiper_index(a_indexes, currentIndex)
+                : prev_swiper_index(a_indexes, currentIndex);
+            currentActive.classList.remove('active');
+            swiper_images.querySelector('[data-featured="' + nextIndex + '"]').classList.add('active');
+        });
+    });
 });
+function prev_swiper_index(numbers, num) {
+    let prev = Number.POSITIVE_INFINITY;
+    let biggest = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < num && (prev === Number.POSITIVE_INFINITY || numbers[i] > prev)) {
+            prev = numbers[i];
+        }
+        if (numbers[i] > num) {
+            biggest = numbers[i];
+        }
+    }
+    return prev === Number.POSITIVE_INFINITY ? biggest : prev;
+}
+function next_swiper_index(numbers, num) {
+    let next = Number.NEGATIVE_INFINITY;
+    let smallest = Number.POSITIVE_INFINITY;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > num && (next === Number.NEGATIVE_INFINITY || numbers[i] < next)) {
+            next = numbers[i];
+        }
+        if (numbers[i] < smallest) {
+            smallest = numbers[i];
+        }
+    }
+    return next === Number.NEGATIVE_INFINITY ? smallest : next;
+}
